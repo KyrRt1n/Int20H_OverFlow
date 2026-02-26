09f3df3e4967826e0b27878fdbf1a278fd3fbe65
K1Desktop <avoloschenko008@gmail.com>
2026-02-26 20:09:42 +0200

    front

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/orders': 'http://localhost:3000',
+      '/tax':    'http://localhost:3000',
+      '/health': 'http://localhost:3000',
+    }
+  }
 })
